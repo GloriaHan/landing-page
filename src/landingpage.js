d247662072406e2da3576c686a8cde5053d9a5d5
Gloria <hanning_hit@hotmail.com>
2023-03-19 20:15:26 +1100

    add mobile menu

--- src/landingpage.js
+++ src/landingpage.js
@@ -1,3 +1,20 @@
+//menu
+const menuToggle = document.querySelector('.menu-toggle')
+const menu = document.querySelector('.menu')
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active')
+})
+
+document.addEventListener('click', (event) => {
+  const isClickInsideMenu = menu.contains(event.target)
+  const isClickInsideMenuToggle = menuToggle.contains(event.target)
+
+  if (!isClickInsideMenu && !isClickInsideMenuToggle) {
+    menu.classList.remove('active')
+  }
+})
+
 // search bar
 const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
